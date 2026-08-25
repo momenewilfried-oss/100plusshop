15e2c3420830bf10b38d8e1c3269f8cefb765d88
momenewilfried-oss <momenewilfried@gmail.com>
2026-08-25 21:09:52 +0100

    connexion

--- js/api.js
+++ js/api.js
@@ -11,7 +11,7 @@ const API_BASE = (function () {
   if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
     return location.origin + '/api';
   }
-  return 'https://100plusshop-backend-pied.vercel.app';;
+  return 'https://100plusshop-backend-pied.vercel.app/api';;
 })();
 
 /**
